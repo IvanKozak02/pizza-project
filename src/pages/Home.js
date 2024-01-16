3415a30b53b635343781894f6ba8bd1dbf428634
Ivan Kozak <ivasykkozak611@gmail.com>
2024-01-16 14:26:50 +0200

    init commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,15 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import pizza from '../assets/images/pizza.jpeg'
 import '../App.css'
-function Home(props) {
+function Home() {
     return (
         <div className="home">
-            <div className="header-container" style={{backgroundImage:`url(${pizza})`}}>
+            <div className="header-container"
+                 style={{
+                     backgroundImage:`url(${pizza})`,
+                     backgroundRepeat: 'no-repeat',
+                     backgroundSize: 'cover'
+            }}>
                 <h1>Ivano`s pizza</h1>
                 <p>THE BEST PIZZA OVER THE WORLD</p>
                 <Link to="/menu">
